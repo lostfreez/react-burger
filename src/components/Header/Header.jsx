a8020ed60029099ccf344428b695fe917b8b0ead
Anton <lostfreez@yandex.ry>
2023-09-11 22:27:19 +0700

    fix: сброс элементов конструктора при переходе на страницу профиля

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,18 @@ import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components"
 import styles from "./Header.module.css";
 import Link from "../Link/Link";
 import { useNavigate } from "react-router-dom";
+import { clearCount } from "../../services/actions/actionsTypes";
+import { clearIngredients } from "../../services/actions/actionsTypes";
+import { useDispatch } from "react-redux";
 
 export default function Header() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  function clearSection() {
+    dispatch(clearCount());
+    dispatch(clearIngredients());
+  }
 
   return (
     <header className={styles.Header}>
@@ -21,7 +30,10 @@ export default function Header() {
       </div>
       <Logo />
       <Link
-        onClick={() => navigate("/profile")}
+        onClick={() => {
+          clearSection();
+          navigate("/profile");
+        }}
         IconComponent={ProfileIcon}
         linkText="Личный&nbsp;кабинет"
       />
