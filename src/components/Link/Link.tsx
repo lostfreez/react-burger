331f4d5b6bdef71f1d3fc8643ba76540ccc0a62d
Anton <lostfreez@yandex.ry>
2023-09-23 19:15:13 +0700

    fix: финальное исправление типов

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -11,29 +11,18 @@ interface Props {
   linkText: string;
 }
 
-const Link: React.FC<Props> = ({
-  onClick,
-  IconComponent,
-  linkText,
-  route,
-}) => {
+const Link: React.FC<Props> = ({ onClick, IconComponent, linkText, route }) => {
   const location = useLocation();
   const isActiveRoute = (route?: string) => {
     return route ? location.pathname === route : false;
   };
 
-  
-
   return (
     <div onClick={onClick} className={styles.link}>
-      <IconComponent
-        type={isActiveRoute(route) ? "primary" : "secondary"}
-      />
+      <IconComponent type={isActiveRoute(route) ? "primary" : "secondary"} />
       <p
         className={`text text_type_main-default ${
-          isActiveRoute(route)
-            ? "text_color_primary"
-            : "text_color_inactive"
+          isActiveRoute(route) ? "text_color_primary" : "text_color_inactive"
         } ml-2`}
       >
         {linkText}
