2ab5ef73ac7dc732cf39a96e7f25cdaba11e9f2b
Anton Troickiy <lostfreez@yandex.ru>
2023-09-29 20:23:29 +0700

    feat: доработать модуль feed

--- src/OrderFeed/OrderFeed.tsx
+++ src/OrderFeed/OrderFeed.tsx
@@ -3,19 +3,19 @@ import styles from "./OrderFeed.module.css";
 function OrderFeed() {
   return (
     <div className={styles.feed}>
-      <div className={`${styles.head} "text text_type_main-large mb-5 mt-10 "`}>
+      <div className={`${styles.head} text text_type_main-large mb-5 mt-10`}>
         Лента заказов
       </div>
       <div className={styles.window}>
         <div className={styles.orders}>
           <div className={styles.order}>
-            <div className={styles.id}>#034535</div>
-            <div className={styles.date}>Сегодня, 16:20 i-GMT+3</div>
-            <div className={styles.burgerName }>
+            <div className={`${styles.id} text text_type_main-medium mt-6 ml-6`}>#034535</div>
+            <div className={`${styles.date} text text_type_main-default mt-6 mr-6`}>Сегодня, 16:20 i-GMT+3</div>
+            <div className={styles.burgerName}>
               Death Star Starship Main бургер Готовится
             </div>
-            <div className={styles.components}>gfdhfgdh</div>
-            <div className={styles.price}>fdghfgdh</div>
+            <div className={styles.components}></div>
+            <div className={styles.price}></div>
           </div>
         </div>
         <div className={styles.stats}>ssss</div>
