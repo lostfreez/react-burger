0a6756d14fdeca1dc2e8eafee683e480d8bcd1a4
Anton <lostfreez@yandex.ry>
2023-10-08 09:40:10 +0700

    refactor: cypress e2e test: для перехода и взаимодействия с элементами теста теперь используется ссылка на элементы через alias, путь тестирования теперь задается в конфиге cypress - baseUrl

--- cypress/e2e/Main.cy.ts
+++ cypress/e2e/Main.cy.ts
@@ -1,41 +1,36 @@
 describe("service is available", function () {
   beforeEach(function () {
-    cy.visit("http://localhost:3000");
+    cy.visit("/");
+    cy.get('[class*="Main_sections"]').as("mainSections");
+    cy.get('[class*="Card_card"]').as("card");
+    cy.get('[class*="SectionConstructor_section"]').as("constructorSection");
   });
-  it("test available on localhost:3000", function () {
-    cy.get('[class*="Main_sections"]').should("be.visible");
+
+  it("test available on baseUrl", function () {
+    cy.get("@mainSections").should("be.visible");
   });
+
   it("test modal", function () {
-    cy.get('[class*="Main_sections"]').should("be.visible");
-    cy.get('[class*="Card_card"]').first().click();
+    cy.get("@mainSections").should("be.visible");
+    cy.get("@card").first().click();
     cy.contains("Детали ингридиента").click();
     cy.get('[class*="Modal_close"]').click();
-    cy.get('[class*="Main_sections"]').should("be.visible");
+    cy.get("@mainSections").should("be.visible");
   });
+
   it("test drag and drop", function () {
     const dataTransfer = new DataTransfer();
-    cy.get('[class*="Card_card"]')
-      .first()
-      .trigger("dragstart", { dataTransfer });
-    cy.get('[class*="SectionConstructor_section"]').trigger("drop", {
-      dataTransfer,
-    });
+    cy.get("@card").first().trigger("dragstart", { dataTransfer });
+    cy.get("@constructorSection").trigger("drop", { dataTransfer });
   });
+
   it("test make order", function () {
     const dataTransfer = new DataTransfer();
-    cy.get('[class*="Main_sections"]').should("be.visible");
-    cy.get('[class*="Card_cardDrag"]')
-      .first()
-      .trigger("dragstart", { dataTransfer });
-    cy.get('[class*="SectionConstructor_section"]').trigger("drop", {
-      dataTransfer,
-    });
-    cy.get('[class*="Card_cardDrag"]')
-      .eq(2)
-      .trigger("dragstart", { dataTransfer });
-    cy.get('[class*="SectionConstructor_section"]').trigger("drop", {
-      dataTransfer,
-    });
+    cy.get("@mainSections").should("be.visible");
+    cy.get("@card").first().trigger("dragstart", { dataTransfer });
+    cy.get("@constructorSection").trigger("drop", { dataTransfer });
+    cy.get("@card").eq(2).trigger("dragstart", { dataTransfer });
+    cy.get("@constructorSection").trigger("drop", { dataTransfer });
     cy.contains("Оформить заказ").click();
     cy.contains("Email").type("test9999@mail.ru");
     cy.contains("Пароль").type("test9999@mail.ru");
